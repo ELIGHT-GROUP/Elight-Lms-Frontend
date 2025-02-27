72028bb072b9d43ed7ad74249bd8ec25c558f2f0
OSHAN AVISHKA <153918928+oshanavishkapiries@users.noreply.github.com>
2025-02-27 13:40:40 +0530

    decker-compose-fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">ELIGHT LMS FRONTEND</h1>
+      <h1>API URL: {import.meta.env.VITE_API_URL}</h1>
       <Button variant="outline">Click me</Button>
     </>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,6 +11,6 @@ export default defineConfig({
     },
   },
   server: {
-    port: 3000,
+    port: 5173,
   },
 });
